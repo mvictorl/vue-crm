d297961de2e90e8aaa480816f157ce01aab28b49
mvictorl <mvictorl@gmail.com>
2020-06-02 15:46:39 +0300

    14:22 CMR на VueJS

---
+++ src/views/Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <form class="card auth-card" @submit.prevent="submitHandler">
+    <div class="card-content">
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input 
+          id="email" 
+          type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
+        />
+        <label for="email">Email</label>
+        
+        <small 
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >
+          Данное поле не может быть пустым
+        </small>
+        <small 
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+        >
+          Введите корректный e-mail
+        </small>
+
+      </div>
+      <div class="input-field">
+        <input 
+          id="password" 
+          type="password" 
+          class="validate" />
+        <label for="password">Пароль</label>
+        
+        <small 
+          class="helper-text invalid"
+
+        >
+          Password        
+        </small>
+
+      </div>
+    </div>
+    <div class="card-action">
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Войти
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Нет аккаунта?
+        <router-link to="/register">Зарегистрироваться</router-link>
+      </p>
+    </div>
+  </form>
+</template>
+
+<script>
+import {email, required, minLength} from 'vuelidate/lib/validators';
+
+export default {
+  name: 'login',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  validations: {
+    email: {required, email},
+    password: {required, minLength: minLength(6)}
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      this.$router.push('/');
+    }
+  }
+}
+</script>
