248c193cf609e14f0f44f77c2a010ba8d7e20d0e
mvictorl <mvictorl@gmail.com>
2020-06-03 11:11:03 +0300

    +

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import firebasw from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/dist';
+
 import dateFilter from "@/filters/date.filter";
 import messagePlugin from "@/utils/message.plugin";
 
@@ -16,6 +20,17 @@ Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 
 Vue.filter("dateFilter", dateFilter);
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyCPC7r3p7xZiRvB-flankrRMQHiqTtHTLs",
+  authDomain: "mvl-vue-crm.firebaseapp.com",
+  databaseURL: "https://mvl-vue-crm.firebaseio.com",
+  projectId: "mvl-vue-crm",
+  storageBucket: "mvl-vue-crm.appspot.com",
+  messagingSenderId: "727202270713",
+  appId: "1:727202270713:web:644b7bde5948ccf4d44128"
+});
 
 new Vue({
   router,
